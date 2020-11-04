1845a29e43c267cd1f190639c6c297b812c3c420
Joshua T. Miltier <jomi3524@colorado.edu>
2020-11-04 15:19:28 -0700

    database schemas

---
+++ database/index.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+// Server Discover and Monitoring engine
+//? If not local, change `localhost` to `database` or IP location of database
+mongoose.connect('mongodb://localhost/advenew', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('[ [ Connected to database. ] ]'))
+  .catch((err) => console.error('Error connecting to database', err));
+
+// mongoDB creates unique IDs, so not added to schema
+const storeSchema = new mongoose.Schema({
+  name: String,
+  logo: String,
+  phoneNumber: String,
+  emailAddress: String,
+  websiteUrl: String,
+  hours: String,
+  calendar: [
+    {
+      datetime: Date, // stores both data and time
+      guide: String,
+      price: Number,
+      booked: Boolean,
+      customerName: String,
+      experience: String,
+      notes: String,
+    },
+  ],
+  store_calendar_request: [
+    {
+      datetime: Date, // stores both data and time
+      guide: String,
+      price: Number,
+      booked: Boolean,
+      customerName: String,
+      experience: String,
+      notes: String,
+    },
+  ],
+});
+
+const customerSchema = new mongoose.Schema({
+  name: String,
+  avatar: String,
+  phoneNumber: String,
+  emailAddress: String,
+});
+
+// compiling schema into a model
+const Store = mongoose.model('Store', storeSchema);
+const Customer = mongoose.model('Customer', customerSchema);
+
+module.exports = { Store, Customer};

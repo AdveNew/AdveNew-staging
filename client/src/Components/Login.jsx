531064833f1f1f675a9944ad5aa4cc9f380f54cc
neel010427 <neelotpalkaturi@gmail.com>
2021-04-01 11:52:53 -0600

    cleaned up login

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,93 +1,22 @@
 /* eslint-disable import/extensions */
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import AppBar from '@material-ui/core/AppBar';
-import Badge from '@material-ui/core/Badge';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import EventIcon from '@material-ui/icons/Event';
-import IconButton from '@material-ui/core/IconButton';
-import MailIcon from '@material-ui/icons/Mail';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import SearchIcon from '@material-ui/icons/Search';
 import TextField from '@material-ui/core/TextField';
-import Toolbar from '@material-ui/core/Toolbar';
-import Tooltip from '@material-ui/core/Tooltip/Tooltip.js';
-import Typography from '@material-ui/core/Typography';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import logo from '../../dist/logo.png';
-
-const useStyles = makeStyles((theme) => ({
-  headerOptions: {
-    flexGrow: 1,
-    marginRight: theme.spacing(2),
-    textAlign: 'center',
-    backgroundColor: 'transparent',
-  },
-  logo: {
-    height: 40,
-    marginRight: theme.spacing(1),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  toolbar: {
-    minHeight: '80',
-  },
-}));
 
 export default function Login(props) {
-  const classes = useStyles();
-  // const [openLogin, setOpenLogin] = React.useState(false);
+  
   const { open, onClose } = props;
-
-  const handleLoginClickOpen = () => {
-    setOpenLogin(true);
-  };
-
-  const handleLoginClose = () => {
-    setOpenLogin(false);
-  };
 
   return (
     <form>
       <div>
-        {/* <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-          Open form dialog
-        </Button> */}
         <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title">
           <DialogTitle id="form-dialog-title">Log In</DialogTitle>
           <DialogContent>
-            {/* <DialogContentText>
-              Log In
-            </DialogContentText> */}
             <TextField
               autoFocus
               margin="dense"
@@ -105,10 +34,10 @@ export default function Login(props) {
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleLoginClose} color="primary">
+            <Button onClick={onClose} color="primary">
               Cancel
             </Button>
-            <Button onClick={handleLoginClose} color="primary">
+            <Button onClick={onClose} color="primary">
               Log In 
             </Button>
           </DialogActions>

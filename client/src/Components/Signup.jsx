6dbb2ab394611cc3b8bbe0f54f381881cb85bcf9
neel010427 <neelotpalkaturi@gmail.com>
2021-04-01 13:11:06 -0600

    check password complete

--- client/src/Components/Signup.jsx
+++ client/src/Components/Signup.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable import/extensions */
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -8,63 +8,112 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
 
 export default function Signup(props) {
-  const { open, onClose } = props;
 
-  return (
+  const { open, onClose } = props;
+  // const passRef = useRef();
+  // const passConfirmRef = useRef();
+  const [pass, setPass] = useState('');
+  const [confirmPass, setConfirmPass] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const passChange = (pass) => {
+    setPass(pass.target.value);
+  };
+
+  const confirmPassChange = (confirmPass) => {
+    setConfirmPass(confirmPass.target.value);
+  };
+
+  const firstNameChange = (firstVal) => {
+    setFirstName(firstVal.target.value);
+  };
+
+  const lastNameChange = (lastVal) => {
+    setLastName(lastVal.target.value);
+  };
+
+  const emailChange = (emailVal) => {
+    setEmail(emailVal.target.value);
+  };
+
+  return(
     <form>
       <div>
-        <Dialog open={open} onClose={onClose} aria-labelledby='form-dialog-title'>
-          <DialogTitle id='form-dialog-title'>Sign Up</DialogTitle>
+        <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">Sign Up</DialogTitle>
           <DialogContent>
             <TextField
               autoFocus
-              margin='dense'
-              id='firstName'
-              label='First Name'
-              type='text'
+              margin="dense"
+              id="firstName"
+              label="First Name"
+              type="text"
+              onChange={firstNameChange}
               fullWidth
               required
             />
             <TextField
-              margin='dense'
-              id='lastName'
-              label='Last Name'
-              type='text'
+              margin="dense"
+              id="lastName"
+              label="Last Name"
+              type="text"
+              onChange={lastNameChange}
               fullWidth
               required
             />
             <TextField
-              margin='dense'
-              id='email'
-              label='Email Address'
-              type='email'
+              margin="dense"
+              id="email"
+              label="Email Address"
+              type="email"
+              onChange={emailChange}
               fullWidth
               required
             />
             <TextField
-              margin='dense'
-              id='password'
-              label='Password'
-              type='password'
+              margin="dense"
+              id="password"
+              label="Password"
+              type="password"
+              // inputRef={passRef}
+              onChange={passChange}
               fullWidth
               required
             />
             <TextField
-              margin='dense'
-              id='repassword'
-              label='Re-enter Password'
-              type='password'
+              margin="dense"
+              id="repassword"
+              label="Re-enter Password"
+              type="password"
+              // inputRef={passConfirmRef}
+              onChange={confirmPassChange}
               fullWidth
               required
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={onClose} color='primary'>
+            <Button onClick={onClose} color="primary">
               Cancel
             </Button>
-            <Button onClick={onClose} color='primary'>
-              Register
-            </Button>
+            {(pass !== '' 
+            && confirmPass !== '' 
+            && pass == confirmPass 
+            && firstName !== '' 
+            && lastName !== '' 
+            && email !== '')
+              ? (
+                <Button onClick={onClose}>
+                  Register
+                </Button>
+              )
+              : (
+                <Button disabled={true}>
+                  Register
+                </Button>
+              )
+            }
           </DialogActions>
         </Dialog>
       </div>

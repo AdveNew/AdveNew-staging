3d810885f77428866b273f39ecbf78d8c31a8752
JMiltier <jomi3524@colorado.edu>
2021-04-05 20:47:39 -0500

    404 page update

--- client/src/Components/NotFound.jsx
+++ client/src/Components/NotFound.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const NotFound = () => (
-  <div>
-    <h3>404 page not found</h3>
-    <p>We are sorry but the page you are looking for does not exist.</p>
+  <div style={{ marginTop: '60px' }}>
+    <h1>404 - Page not found</h1>
+    <h3>We are sorry but the page you are looking for does not exist.</h3>
   </div>
 );
 

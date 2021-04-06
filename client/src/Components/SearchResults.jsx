22388c2a5370b31e3f4a2c5a75faf3e49980c819
JMiltier <jomi3524@colorado.edu>
2021-04-05 20:44:21 -0500

    update time formatting for search results

--- client/src/Components/SearchResults.jsx
+++ client/src/Components/SearchResults.jsx
@@ -5,6 +5,7 @@ import {
   CircularProgress,
   Backdrop,
 } from '@material-ui/core';
+import { format, parseISO } from 'date-fns';
 import { DataGrid } from '@material-ui/data-grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Payment from './Payment.jsx';
@@ -37,7 +38,18 @@ export default function SearchResults(props) {
     { field: 'guide', headerName: 'Guide\'s Name', flex: 0.8 },
     { field: 'location', headerName: 'Location', flex: 1 },
     {
-      field: 'startDate', headerName: 'Date', type: 'dateTime', flex: 1,
+      field: 'startDate',
+      headerName: 'Start',
+      type: 'dateTime',
+      flex: 1,
+      renderCell: (d) => format(parseISO(d.value), 'EEE MMM-dd HH:mm b'),
+    },
+    {
+      field: 'endDate',
+      headerName: 'End',
+      type: 'dateTime',
+      flex: 1,
+      renderCell: (d) => format(parseISO(d.value), 'EEE MMM-dd HH:mm b'),
     },
     { field: 'groupSize', headerName: 'Group Size', flex: 0.8 },
     { field: 'experience', headerName: 'Experience', flex: 0.8 },
